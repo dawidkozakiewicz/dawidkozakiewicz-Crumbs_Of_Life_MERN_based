91d72a21c7e176bc231fdfdf151207fc635be504
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-03-18 23:34:15 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+//nowoczesny typ zapisu powyżej możliwy jest dzięki " "type": "module"" w package.json" 
