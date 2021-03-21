8a05be80cf2b5cb010abfd566444a8936d3c0175
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-03-21 18:54:34 +0100

    starting setup, pages and routes

--- CRUMBS_OF_LIFE/src/App.js
+++ CRUMBS_OF_LIFE/src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch> {/* gdyby nie switch, nastąpiłaby egzekucja kodu w dół i route wróciłby do "/" */}
+        <Route path="/" exact> {/* bez exact renderowałoby wszystko, co zaczyna się na "/" */}
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
