60130a33df2f6b909817789369f6f24f7f5e55e1
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-04-05 19:26:56 +0200

    further work on the project map

--- CRUMBS_OF_LIFE/src/places/pages/UserPlaces.js
+++ CRUMBS_OF_LIFE/src/places/pages/UserPlaces.js
@@ -34,8 +34,8 @@ const DUMMY_PLACES = [
 
 const UserPlaces = () => {
   const userId = useParams().userId;
-  console.log(useParams())
   const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  console.log(loadedPlaces)
   return <PlaceList items={loadedPlaces} />;
 };
 
