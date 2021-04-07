b113e5c05343c20708d5fea661738bfe6fe215ed
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-04-07 22:10:17 +0200

    adding custom buttons

--- CRUMBS_OF_LIFE/src/user/pages/Users.js
+++ CRUMBS_OF_LIFE/src/user/pages/Users.js
@@ -10,15 +10,7 @@ const Users = () => {
       image:
         'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
       places: 3
-    },
-    {
-      id: 'u3',
-      name: 'Ernst Schwanz',
-      image:
-        'https://cdn.shopify.com/s/files/1/0638/6243/products/32_245aed8a-e009-4036-b98f-7ca22a9edb90_1024x1024.jpg?v=1571438588',
-      places: 3
     }
-
   ];
 
   return <UsersList items={USERS} />;
