684edfa2d114669d45c9592e8d1451873b28e708
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-04-24 19:27:50 +0200

    add new free map provider

--- CRUMBS_OF_LIFE/src/places/pages/UserPlaces.js
+++ CRUMBS_OF_LIFE/src/places/pages/UserPlaces.js
@@ -6,14 +6,14 @@ import PlaceList from '../components/PlaceList';
 const DUMMY_PLACES = [
   {
     id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
+    title: 'Wrocław',
+    description: 'The City Of Gnomes',
     imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-    address: '20 W 34th St, New York, NY 10001',
+      'https://ocdn.eu/pulscms-transforms/1/ksTk9kpTURBXy9hZmZmNGU1ODBjZmI5NGI4YTRmZWZlNjIxMTNjMzgzYy5qcGeTlQPNAe4AzRvHzQ-gkwXNAxTNAbyTCaY2MjI0MDAGgaEwAQ/wroclaw.jpg',
+    address: 'Wrocław, Rynek, Poland',
     location: {
-      lat: 40.7484405,
-      lng: -73.9878584
+      lat: 51.1100488,
+      lng: 17.0307688
     },
     creator: 'u1'
   },
